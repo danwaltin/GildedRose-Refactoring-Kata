9bfe6d3c79eb561a93acf4e88644275ca5fc488c
Dan Waltin <dan@waltin.se>
2025-01-23 10:25:43 +0100

    starting on tests

--- swift/Tests/GildedRoseTests/GildedRoseTests.swift
+++ swift/Tests/GildedRoseTests/GildedRoseTests.swift
@@ -1,11 +1,88 @@
 @testable import GildedRose
-import XCTest
 
-class GildedRoseTests: XCTestCase {
-    func testFoo() throws {
-        let items = [Item(name: "foo", sellIn: 0, quality: 0)]
-        let app = GildedRose(items: items)
-        app.updateQuality()
-        XCTAssertEqual(app.items[0].name, "fixme")
-    }
+import Testing
+
+struct DexterityVest {
+
+	@Test
+	func nameIsSame() {
+		let items = [Item(name: "+5 Dexterity Vest", sellIn: 5, quality: 5)]
+		let app = GildedRose(items: items)
+		
+		#expect(app.items[0].name == "+5 Dexterity Vest")
+
+		app.updateQuality()
+		
+		#expect(app.items[0].name == "+5 Dexterity Vest")
+
+		app.updateQuality()
+		
+		#expect(app.items[0].name == "+5 Dexterity Vest")
+	}
+
+	@Test
+	func startingAtZero_day0() {
+		let items = [Item(name: "+5 Dexterity Vest", sellIn: 0, quality: 0)]
+		let app = GildedRose(items: items)
+		
+		#expect(app.items[0].sellIn == 0)
+		#expect(app.items[0].quality == 0)
+	}
+	
+	@Test
+	func sellIn_quality_different_day0() {
+		let items = [Item(name: "+5 Dexterity Vest", sellIn: 8, quality: 16)]
+		let app = GildedRose(items: items)
+		
+		#expect(app.items[0].sellIn == 8)
+		#expect(app.items[0].quality == 16)
+	}
+	
+	@Test
+	func sellIn_quality_day1() {
+		let items = [Item(name: "+5 Dexterity Vest", sellIn: 8, quality: 16)]
+		let app = GildedRose(items: items)
+		
+		app.updateQuality()
+		
+		#expect(app.items[0].sellIn == 7)
+		#expect(app.items[0].quality == 15)
+	}
+	
+	@Test
+	func qualityDecreasesTwiceAsFastAfterSellDate() {
+		let items = [Item(name: "+5 Dexterity Vest", sellIn: 1, quality: 6)]
+		let app = GildedRose(items: items)
+		
+		app.updateQuality()
+		
+		#expect(app.items[0].sellIn == 0)
+		#expect(app.items[0].quality == 5)
+		
+		app.updateQuality()
+		
+		#expect(app.items[0].sellIn == -1)
+		#expect(app.items[0].quality == 3)
+		
+		app.updateQuality()
+		
+		#expect(app.items[0].sellIn == -2)
+		#expect(app.items[0].quality == 1)
+	}
+	
+	@Test
+	func qualityNeverGoUnder0() {
+		let items = [Item(name: "+5 Dexterity Vest", sellIn: 5, quality: 1)]
+		let app = GildedRose(items: items)
+		
+		app.updateQuality()
+		
+		#expect(app.items[0].sellIn == 4)
+		#expect(app.items[0].quality == 0)
+
+		app.updateQuality()
+		
+		#expect(app.items[0].sellIn == 3)
+		#expect(app.items[0].quality == 0)
+	}
 }
