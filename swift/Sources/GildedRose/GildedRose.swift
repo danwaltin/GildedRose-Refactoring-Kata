a06e238264876b706167b9ab8aaa9ef5ae66f8a8
Dan Waltin <dan@waltin.se>
2025-01-23 13:50:11 +0100

    refactored handling of backstage passes

--- swift/Sources/GildedRose/GildedRose.swift
+++ swift/Sources/GildedRose/GildedRose.swift
@@ -11,7 +11,7 @@ public class GildedRose {
 	
 	private func updateItem(_ item: Item) -> Item {
 		var updated = item
-
+		
 		if updated.name == "Sulfuras, Hand of Ragnaros" {
 			return updated
 		}
@@ -29,26 +29,31 @@ public class GildedRose {
 				}
 			}
 			return updated
-
+			
 		} else if updated.name == "Backstage passes to a TAFKAL80ETC concert" {
 			if updated.quality < 50 {
 				updated.quality = updated.quality + 1
 				
-				if updated.name == "Backstage passes to a TAFKAL80ETC concert" {
-					if updated.sellIn < 11 {
-						if updated.quality < 50 {
-							updated.quality = updated.quality + 1
-						}
+				if updated.sellIn < 11 {
+					if updated.quality < 50 {
+						updated.quality = updated.quality + 1
 					}
-					
-					if updated.sellIn < 6 {
-						if updated.quality < 50 {
-							updated.quality = updated.quality + 1
-						}
+				}
+				
+				if updated.sellIn < 6 {
+					if updated.quality < 50 {
+						updated.quality = updated.quality + 1
 					}
 				}
 			}
 
+			updated.sellIn = updated.sellIn - 1
+
+			if updated.sellIn < 0 {
+				updated.quality = 0
+			}
+			
+			return updated
 		} else {
 			if updated.quality > 0 {
 				updated.quality = updated.quality - 1
@@ -58,13 +63,9 @@ public class GildedRose {
 		updated.sellIn = updated.sellIn - 1
 		
 		if updated.sellIn < 0 {
-				if updated.name == "Backstage passes to a TAFKAL80ETC concert" {
-					updated.quality = 0
-				} else {
-					if updated.quality > 0 {
-						updated.quality = updated.quality - 1
-					}
-				}
+			if updated.quality > 0 {
+				updated.quality = updated.quality - 1
+			}
 		}
 		return updated
 	}
